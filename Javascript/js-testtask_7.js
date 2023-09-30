99d4d2d085b16b88ca4873e94bef665fb6dc945e
tania-it <tania.it-courses@mail.ru>
2023-09-30 20:04:10 +0300

    find duplicaties function

---
+++ Javascript/js-testtask_7.js
@@ -0,0 +1,26 @@
+
+//const duplicatedSymbols = findDuplicates(string1, string2); 
+
+const string1 = 'Hello';
+const string2 = 'Welcome';
+        for (var i = 0; i < string2.length; i++) {
+            var stringresult = ''; 
+if (  string1[i] === string2[i]) {
+        stringresult = stringresult + string2[i];
+        console.log (stringresult);
+        
+} else  {
+   stringresult = 'empty string';
+   console.log (stringresult);
+}
+}
+
+
+//const duplicatedSymbols = findDuplicates(string1, string2);
+
+//.toLowerCase()
+// string1.indexOf ()
+// split
+// replace
+
+
