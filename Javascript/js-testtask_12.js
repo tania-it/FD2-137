5e21bcca9488bc9a0a18b2edefdf76704272695c
tania-it <tania.it-courses@mail.ru>
2023-10-10 21:52:31 +0300

    did tasks for practice

---
+++ Javascript/js-testtask_12.js
@@ -0,0 +1,63 @@
+function FirstFunction() {
+
+    for (let i = 1; i <= 50; i++) {
+        console.log(i);
+    }
+
+};
+
+function SecondFunction(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function ThirdFunction(arr) {
+    let prod = 1;
+    for (let i = 0; i < arr.length; i++) {
+        prod *= arr[i];
+    }
+    return prod;
+}
+
+console.log(prod);
+
+
+function fourthFunction(arr) {
+    return arr.filter((el) => (el % 2 === 0) && (0 <= el) && (el <= 100));
+}
+console.log(arr);
+console.log(fourthFunction);
+
+function fiveFunction(number) {
+    let i = 0;
+    while (number >= 50) {
+        number = number / 2;
+        i = i + 1;
+    }
+    return i;
+}
+console.log(fiveFunction(1000));
+
+function sixFunction (number) {
+    let arr = [];
+    let number = 'number'.split('');
+    let sumcifr = 0;
+
+    for (let number of arr) {
+        if ()
+    }
+}
+
+
+function getNumbersSum(num){
+    
+      var result = 'num'.split('');
+    
+      alert(result);
+    
+    }
+    
+    getNumbersSum()
+    
