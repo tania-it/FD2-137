e3a7fbf04e802ee93789cbfe3891e55c24fbe67a
tania-it <tania.it-courses@mail.ru>
2023-10-19 21:58:56 +0300

    did requestAnimationFrame function

--- Javascript/lesson Async & promise/index.js
+++ Javascript/lesson Async & promise/index.js
@@ -6,14 +6,48 @@ setTimeout(() => {
     clearTimeout(timerId);
     setTimeout(() => {
         console.log('6');
-    clearInterval(intervalId);    
+        clearInterval(intervalId);
     }, 3_000);
 }, 3_000);
 
 
 let s = 0;
- 
+
 const intervalId = setInterval(() => {
     console.log(++s);
 }, 1_000);
 
+
+const cancelTask = scheduleTask(() => {
+    console.log('task');
+});
+
+
+function scheduleTask(task) {
+
+  const id = setTimeout(() => task(), 300); 
+        cancelTask();
+    return () => clearTimeout(id);
+    };
+
+
+/*queueMicrotask(() => {
+
+}) -     
+*/
+
+const stop = renderCycle( () => {
+
+});
+
+function renderCycle(callback) {
+    const id = requestAnimationFrame(() => {
+            callback;
+            requestAnimationFrame 
+    });
+
+    return () => {
+        cancelAnimationFrame(id);
+    };
+
+}
