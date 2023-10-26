229b71745186073ba82c6c388ef506124b404a66
tania-it <tania.it-courses@mail.ru>
2023-10-26 21:46:46 +0300

    did index.js function-generation

---
+++ Javascript/lesson Function-generation/index.js
@@ -0,0 +1,58 @@
+function* getValues (symbols, repeatCount) {
+    for (const symbol of symbols) {
+        {
+            yield* repeatTimes(symbol,repeatCount);
+        }
+    }
+}
+
+function* repeatTimes(value, repeatCount) {
+    for(let i = 1; i <= repeatCount; i++){
+        yield value;
+}
+}
+
+const iterableObject = {
+    values: [1,2,3],
+    [Symbol.iterator]() {
+        return this.values.values();
+    }
+};
+
+/*const iterableObject = {
+    values: [1,2,3],
+    *[Symbol.iterator]() {
+        yield* this.values.values();
+    }
+};
+*/
+
+class Test {
+    *[Symbol.iterator]() {
+        yield1;
+        yield2;
+        yield3;
+    }
+} 
+
+[Symbol.toPrimitive](hint) {
+    switch (hint) {
+        case 'number':
+            return 5;
+        default:
+            return 'five';    
+    }
+}
+
+
+const _disabled = new WeakMap();
+ class Test {
+    constructor() {
+        _disabled.set (this, false);
+
+    }
+ }
+ get disabled() {
+    _disabled.get(this);
+ }
+ 
