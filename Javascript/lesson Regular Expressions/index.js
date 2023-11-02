b7ef905abe88abe813e43ea44e11c07691a4946e
tania-it <tania.it-courses@mail.ru>
2023-11-03 02:43:16 +0300

    fixed index.js RegularExpression

--- Javascript/lesson Regular Expressions/index.js
+++ Javascript/lesson Regular Expressions/index.js
@@ -1,3 +1,4 @@
+
 function replaceLongColors(text) {
     return text.replace(/#([\da-f])\1([\da-f])\2([\da-f])\3/gi,
         (_, r, g, b) => `#${r}${g}${b}`,
@@ -11,8 +12,33 @@ function toCamelCase(text) {
         return match.charAt(1).toUpperCase();
     });
 }
+//Метод charAt() возвращает символ по указанному индексу в строке.
 
 function toCamelCase2(text) {
     return text.replace(/_([a-z])/gi, (_, letter) => letter.toUpperCase());
 }
+
+// How to check if character is a letter in Javascript?
+// как проверить, является ли символ буквой в Javascript?
+
+function isLetter(c) {
+    return c.toLowerCase() != c.toUpperCase();
+  }
+
+
+  let cat = {
+    name: 'Tom',
+    isHunter: true
+  }
+  
+  let mouse = {
+    name: 'Jerry',
+    isHunter: false
+  }
+  
+  mouse = cat;
+  cat.isHunter = false;
+  mouse.isHunter = undefined;
+  
+  console.log(mouse.isHunter);
 
