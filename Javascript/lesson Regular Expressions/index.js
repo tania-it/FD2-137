2ccafb1ce6695c4814b98b78e1f1fdb94e782ea6
tania-it <tania.it-courses@mail.ru>
2023-11-02 22:04:24 +0300

    did index regular expression

--- Javascript/lesson Regular Expressions/index.js
+++ Javascript/lesson Regular Expressions/index.js
@@ -1,20 +1,18 @@
 function replaceLongColors(text) {
-    return text.replace (/#([\da-f])\1([\da-f])\2([\da-f])\3/gi,
-     (_, r, g, b) =>  `#${r}${g}${b}`,
+    return text.replace(/#([\da-f])\1([\da-f])\2([\da-f])\3/gi,
+        (_, r, g, b) => `#${r}${g}${b}`,
 
     );
 }
 
 
 function toCamelCase(text) {
-    return text.replace (/_[a-z])/gi, 
-        (match) => {
-           match.charAt(1).toUpperCase();
-        }
-    )
-    
+    return text.replace(/_[a-z]/gi, (match) => {
+        return match.charAt(1).toUpperCase();
+    });
+}
+
 function toCamelCase2(text) {
-    return text.replace (/_[a-z])/gi, 
-        (_, letter) => {
-            letter.toUpperCase()};
-        }
+    return text.replace(/_([a-z])/gi, (_, letter) => letter.toUpperCase());
+}
+
