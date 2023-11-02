89e586f91f7614f10ae8d0ce213aa1eff75564d5
tania-it <tania.it-courses@mail.ru>
2023-11-02 21:58:14 +0300

    did task regular expression

---
+++ Javascript/lesson Regular Expressions/index.js
@@ -0,0 +1,20 @@
+function replaceLongColors(text) {
+    return text.replace (/#([\da-f])\1([\da-f])\2([\da-f])\3/gi,
+     (_, r, g, b) =>  `#${r}${g}${b}`,
+
+    );
+}
+
+
+function toCamelCase(text) {
+    return text.replace (/_[a-z])/gi, 
+        (match) => {
+           match.charAt(1).toUpperCase();
+        }
+    )
+    
+function toCamelCase2(text) {
+    return text.replace (/_[a-z])/gi, 
+        (_, letter) => {
+            letter.toUpperCase()};
+        }
