b4a0c3141aeb2cb3532c5126ce56de50c952a23f
tania-it <tania.it-courses@mail.ru>
2023-11-30 20:23:36 +0300

    did task with regexp on lesson

--- Javascript/lesson Regular Expressions/index.js
+++ Javascript/lesson Regular Expressions/index.js
@@ -42,3 +42,11 @@ function isLetter(c) {
   
   console.log(mouse.isHunter);
 
+
+  
+function t(text, data) {
+    return text.replace(/{{(\w+)}}/g, (match, key) => {
+        return Object.hasOwn(data, key) ? String(data[key]) : match;    
+    });
+}
+
